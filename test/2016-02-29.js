6335de2f50601fe630f2ae6c9a8934ce449979d9
Sun Lifeng <1104506375@qq.com>
2016-02-29 18:22:46 +0800

    xx

--- test/2016-02-29.js
+++ test/2016-02-29.js
@@ -60,15 +60,44 @@
 // f1();
 // alert(n);
 
-function a(){
-    var i=0;
-    function b(){
-        alert(i);
-        i++;
-    }
-    return b;
-}
-var c=a();
-c(); // 0
-c(); // 1
+// function a(){
+//     var i=0;
+//     function b(){
+//         alert(i);
+//         i++;
+//     }
+//     return b;
+// }
+// var c=a();
+// c(); // 0
+// c(); // 1
 
+function Foo() {
+    getName = function() {
+        alert(1);
+    };
+    return this;
+}
+Foo.getName = function() {
+    alert(2);
+};
+Foo.prototype.getName = function() {
+    alert(3);
+};
+var getName = function() {
+    alert(4);
+}
+
+function getName() {
+    alert(5);
+}
+
+// 写出一下的输出
+Foo.getName();  // 2
+getName();      // 4
+Foo().getName() // 1
+getName();      // 1
+
+new Foo.getName();  // 2
+new Foo().getName();  // 3
+new new Foo().getName(); //3
